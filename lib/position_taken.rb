52d9168591e4039b3198c752172404f63f81cdd8
Min Hee  Park <minheekangg@gmail.com>
2018-08-30 00:05:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken(board, index)
-  if board[index] == " " || board[index] == "" ||  board[index] == nil
-    return false
-  else
+  if board[index] == "X" || board[index] == "O"
     return true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return false
   end
 end
